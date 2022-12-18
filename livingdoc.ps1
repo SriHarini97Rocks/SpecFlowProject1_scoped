08736340053fc5b1b439a6d15e69bf8b2a7e76ae
Manikandan <manikandanm441@gmail.com>
2022-12-18 19:49:51 +0530

    change 4

--- livingdoc.ps1
+++ livingdoc.ps1
@@ -1,2 +1,2 @@
-taskkill /IM cmd.exe
+taskkill /IM cmd.exe /f
 livingdoc test-assembly "$TestAssemblyPath" --test-execution-json "$TestExecutionPath" --output-type HTML --output "$HtmlReportPath" --title "$TestTitle"
